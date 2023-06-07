56b91c562648091a5ec7ae9d2540799e99ad7353
AyakaB <ayaka.bando22@gmail.com>
2023-06-07 06:15:07 -0700

    added theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+          <div className="app">
+            <main className="content"></main>
+          </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
