9ac331717282f9ad8a1ce8a55b0fa6e3c578c286
AyakaB <ayaka.bando22@gmail.com>
2023-06-08 08:54:58 -0700

    header

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import Header from '../../components/Header'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
+    <Box m="20px">
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Header title="DASHBOARD" subtitle="Welcom to your Dashboard" />
+      </Box>
+    </Box>
+    )
 }
 
 export default Dashboard
