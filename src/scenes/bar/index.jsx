e678f054e7dcff998f9d96bbded6e1f68e8fd32e
AyakaB <ayaka.bando22@gmail.com>
2023-06-11 23:23:52 -0700

    pie chart

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import Header from '../../components/Header'
+import BarChart from '../../components/BarChart'
 
-const index = () => {
+const Bar = () => {
   return (
-    <div>index</div>
+    <Box m="20px">
+      <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+        <Box height="75vh">
+          <BarChart />
+        </Box>
+    </Box>
   )
 }
 
-export default index
+export default Bar
--- src/scenes/pie/index.jsx
+++ src/scenes/pie/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import Header from '../../components/Header'
+import PieChart from '../../components/PieChart'
 
-const index = () => {
+const Pie = () => {
   return (
-    <div>index</div>
+    <Box m="20px">
+      <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+        <Box height="75vh">
+          <PieChart />
+        </Box>
+    </Box>
   )
 }
 
-export default index
+export default Pie
